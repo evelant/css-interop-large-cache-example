06ccef780a1e37276b2eb5ae1bb2e3009ef98d12
evelant <andrew@taskheroics.com>
2024-09-14 08:41:11 -0400

    example of large css-interop-cache

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
+import { Text, View } from 'react-native';
 import './global.css';
 
 export default function App() {
   return (
     <>
       <ScreenContent title="Home" path="App.tsx" />
+      <View className="bg-red-500">
+        <Text>foo</Text>
+      </View>
       <StatusBar style="auto" />
     </>
   );
